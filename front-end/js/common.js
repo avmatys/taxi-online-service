0275d273eeff8c156731a8228a2438e6d73b3051
zaruda <zaruda46@gmail.com>
2017-04-15 19:46:05 +0300

    init commit

---
+++ front-end/js/common.js
@@ -0,0 +1,44 @@
+$(function() {
+	$('.logo').click(function(){
+		window.location="index.html";
+	})
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+
+	$("form").submit(function() { 
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", 
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+});
+function redirectTo(link){
+	window.location = ''+link;
+}
