0761e73ddf3126a66f393d43420a090f82d045b3
andrey <andrey_matys@mail.ru>
2017-04-23 15:43:23 +0300

    Added authorization client future

---
+++ taxi-online-service/front-end/js/common.js
@@ -0,0 +1,29 @@
+/*Common js functions*/
+$(function() {
+	$('.logo').click(function(){
+		window.location="index.html";
+	})
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+});
+function redirectTo(link){
+	window.location = ''+link;
+}
