fc69494fef74257d00b9a2c736c4823617af14a9
andrey <andrey_matys@mail.ru>
2017-04-23 15:48:45 +0300

    Revert "Added authorization client future"
    This reverts commit 0761e73ddf3126a66f393d43420a090f82d045b3.

--- taxi-online-service/front-end/js/login.js
+++
@@ -1,8 +0,0 @@
-// Add event handler for submit form event
-// Functions are described in file utils.js
-jQuery(document).ready(function ($) {
-    $('#form_login').submit(function (event) {
-        event.preventDefault();
-        authorization(createAuthorizationJSONData());
-    });
-});
