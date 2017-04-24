b4e165487d4e1fbf3b12a51ddfd80bd1ef73ab90
andrey <andrey_matys@mail.ru>
2017-04-24 16:46:08 +0300

    Fixed alert

--- front-end/js/common.js
+++ front-end/js/common.js
@@ -1,39 +1,35 @@
 /*Common js functions*/
-$(function() {
-	$('.logo').click(function(){
-		window.location="index.html";
-	});
+jQuery(document).ready(function ($) {
+    $('.logo').click(function(){
+        window.location="index.html";
+    });
 
-	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
-			return $(this).attr("src").replace(".svg", ".png");
-		});
-	}
+    //SVG Fallback
+    if(!Modernizr.svg) {
+        $("img[src*='svg']").attr("src", function() {
+            return $(this).attr("src").replace(".svg", ".png");
+        });
+    }
 
-	//Chrome Smooth Scroll
-	try {
-		$.browserSelector();
-		if($("html").hasClass("chrome")) {
-			$.smoothScroll();
-		}
-	} catch(err) {
+    //Chrome Smooth Scroll
+    try {
+        $.browserSelector();
+        if($("html").hasClass("chrome")) {
+            $.smoothScroll();
+        }
+    } catch(err) {
 
-	}
+    }
 
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+    $("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
 
 });
+
 function redirectTo(link){
 	window.location = ''+link;
 }
-var close = document.getElementsByClassName("closebtn");
-var i;
 
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function(){
-        var div = this.parentElement;
-        div.style.opacity = "0";
-        setTimeout(function(){ div.style.display = "none"; }, 600);
-    }
+function showHide(){
+	$(this).parent().toggle();
 }
