eabd30c288ff3b29aa39f5c77cfff6aaa267a6b3
zaruda <zaruda46@gmail.com>
2017-04-24 18:24:10 +0300

    release of user's and driver's profiles

--- front-end/js/common.js
+++ front-end/js/common.js
@@ -2,7 +2,16 @@
 $(function() {
 	$('.logo').click(function(){
 		window.location="index.html";
-	});
+	})
+	$('.dropdown-toggle').click(function () {
+	    if($(this).hasClass('active')){
+            $(this).css('transform','rotate(-45deg)').removeClass('active');
+        }else{
+            $(this).css('transform','rotate(45deg)').addClass('active');
+        }
+
+
+    })
 
 	//SVG Fallback
 	if(!Modernizr.svg) {
