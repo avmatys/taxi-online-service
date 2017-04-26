211a03a81e904f8745fa3be727819c3c887d7811
zaruda <zaruda46@gmail.com>
2017-04-26 19:40:00 +0300

    common.js fx

--- front-end/js/common.js
+++ front-end/js/common.js
@@ -1,41 +1,17 @@
 /*Common js functions*/
+jQuery(document).ready(function ($) {
+    $('.logo').click(function(){
+        window.location="index.html";
+    });
 
-$(function() {
-	$('.logo').click(function(){
-		window.location="index.html";
-	});
-	$('.dropdown-toggle').click(function () {
-	    if($(this).hasClass('active')){
+    $('.dropdown-toggle').click(function () {
+        if($(this).hasClass('active')){
             $(this).css('transform','rotate(-45deg)').removeClass('active');
         }else{
             $(this).css('transform','rotate(45deg)').addClass('active');
         }
 
 
-    });
-
-	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
-			return $(this).attr("src").replace(".svg", ".png");
-		});
-	}
-
-	//Chrome Smooth Scroll
-	try {
-		$.browserSelector();
-		if($("html").hasClass("chrome")) {
-			$.smoothScroll();
-		}
-	} catch(err) {
-
-	}
-
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
-
-jQuery(document).ready(function ($) {
-    $('.logo').click(function(){
-        window.location="index.html";
     });
 
     //SVG Fallback
@@ -62,9 +38,9 @@ jQuery(document).ready(function ($) {
 });
 
 function redirectTo(link){
-	window.location = ''+link;
+    window.location = ''+link;
 }
 
-function showHide(){
-	$(this).parent().toggle();
+function toggle(){
+    $(this).parent().toggle();
 }
