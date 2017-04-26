f5b0e8fa748d5f63cda9f9a945941acd2b83f5b5
zaruda <zaruda46@gmail.com>
2017-04-26 19:50:47 +0300

    fixed

--- front-end/js/common.js
+++ front-end/js/common.js
@@ -1,46 +1,22 @@
 /*Common js functions*/
 
 $(function() {
-	$('.logo').click(function(){
-		window.location="index.html";
-	});
-	$('.dropdown-toggle').click(function () {
-	    if($(this).hasClass('active')){
-            $(this).css('transform','rotate(-45deg)').removeClass('active');
-        }else{
-            $(this).css('transform','rotate(45deg)').addClass('active');
+    $('.logo').click(function () {
+        window.location = "index.html";
+    });
+    $('.dropdown-toggle').click(function () {
+        if ($(this).hasClass('active')) {
+            $(this).css('transform', 'rotate(-45deg)').removeClass('active');
+        } else {
+            $(this).css('transform', 'rotate(45deg)').addClass('active');
         }
 
 
     });
 
-	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
-			return $(this).attr("src").replace(".svg", ".png");
-		});
-	}
-
-	//Chrome Smooth Scroll
-	try {
-		$.browserSelector();
-		if($("html").hasClass("chrome")) {
-			$.smoothScroll();
-		}
-	} catch(err) {
-
-	}
-
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
-
-jQuery(document).ready(function ($) {
-    $('.logo').click(function(){
-        window.location="index.html";
-    });
-
     //SVG Fallback
-    if(!Modernizr.svg) {
-        $("img[src*='svg']").attr("src", function() {
+    if (!Modernizr.svg) {
+        $("img[src*='svg']").attr("src", function () {
             return $(this).attr("src").replace(".svg", ".png");
         });
     }
@@ -48,23 +24,50 @@ jQuery(document).ready(function ($) {
     //Chrome Smooth Scroll
     try {
         $.browserSelector();
-        if($("html").hasClass("chrome")) {
+        if ($("html").hasClass("chrome")) {
             $.smoothScroll();
         }
-    } catch(err) {
+    } catch (err) {
 
     }
 
-    $("img, a").on("dragstart", function(event) { event.preventDefault(); });
+    $("img, a").on("dragstart", function (event) {
+        event.preventDefault();
+    });
 
 
+    $('.logo').click(function () {
+        window.location = "index.html";
+    });
 
+    //SVG Fallback
+    if (!Modernizr.svg) {
+        $("img[src*='svg']").attr("src", function () {
+            return $(this).attr("src").replace(".svg", ".png");
+        });
+    }
+
+    //Chrome Smooth Scroll
+    try {
+        $.browserSelector();
+        if ($("html").hasClass("chrome")) {
+            $.smoothScroll();
+        }
+    } catch (err) {
+
+    }
+
+    $("img, a").on("dragstart", function (event) {
+        event.preventDefault();
+    });
 });
 
-function redirectTo(link){
-	window.location = ''+link;
-}
 
-function showHide(){
-	$(this).parent().toggle();
-}
+    function redirectTo(link) {
+        window.location = '' + link;
+    }
+
+    function showHide() {
+        $(this).parent().toggle();
+    }
+
