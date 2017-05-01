144d4d119260b05291ad558d9262b7515ecb0d06
ilyazarudny <zaruda46@gmail.com>
2017-05-01 22:24:16 +0300

    common.js fix

--- front-end/js/common.js
+++ front-end/js/common.js
@@ -10,8 +10,13 @@ $(function() {
         } else {
             $(this).css('transform', 'rotate(45deg)').addClass('active');
         }
+    });
+    $(document).on('click', 'a[href*=#]', function(event){
+        event.preventDefault();
 
-
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 750);
     });
 
     //SVG Fallback
@@ -35,39 +40,12 @@ $(function() {
         event.preventDefault();
     });
 
-
-    $('.logo').click(function () {
-        window.location = "index.html";
-    });
-
-    //SVG Fallback
-    if (!Modernizr.svg) {
-        $("img[src*='svg']").attr("src", function () {
-            return $(this).attr("src").replace(".svg", ".png");
-        });
-    }
-
-    //Chrome Smooth Scroll
-    try {
-        $.browserSelector();
-        if ($("html").hasClass("chrome")) {
-            $.smoothScroll();
-        }
-    } catch (err) {
-
-    }
-
-    $("img, a").on("dragstart", function (event) {
-        event.preventDefault();
-    });
 });
 
+function redirectTo(link){
+	window.location = ''+link;
+}
 
-    function redirectTo(link) {
-        window.location = '' + link;
-    }
-
-    function showHide() {
-        $(this).parent().toggle();
-    }
-
+function showHide(){
+	$(this).parent().toggle();
+}
